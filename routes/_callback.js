8150461e74afc1737f500fbd82f0b86c3c0cf895
Leonardo Matos <leomp120894@gmail.com>
2019-06-07 18:16:11 -0300

    style: br fixes for promise handlers

--- routes/_callback.js
+++ routes/_callback.js
@@ -5,10 +5,12 @@ const logger = require('console-files')
 
 const POST = (id, meta, body, respond, storeId, appSdk) => {
   // Store API authentication callback
-  appSdk.handleCallback(storeId, body).then(() => {
-    // just respond first
-    respond(null, null, 204)
-  })
+  appSdk.handleCallback(storeId, body)
+
+    .then(() => {
+      // just respond first
+      respond(null, null, 204)
+    })
 
     .catch(err => {
       if (typeof err.code === 'string' && !err.code.startsWith('SQLITE_CONSTRAINT')) {
--- routes/procedure.js
+++ routes/procedure.js
@@ -1,1 +1,24 @@
 'use strict'
+
+// read configured E-Com Plus app data
+const getConfig = require('./../lib/Api/AppConfig')
+
+const POST = (id, meta, trigger, respond, storeId, appSdk) => {
+  // get user notification options from E-Com Plus application data
+  getConfig({ appSdk, storeId })
+
+    .then(({ authentications }) => {
+      if (authentications) {
+      }
+    })
+
+    .catch(err => {
+      // cannot read app data
+      // return error status code
+      respond({}, null, 500, 'APP_ERR', err.message)
+    })
+}
+
+module.exports = {
+  POST
+}
