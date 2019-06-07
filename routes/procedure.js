5984f089db77e3b4275c3b80ed9a5fa6d3c4d2e2
Leonardo Matos <leomp120894@gmail.com>
2019-06-07 19:13:20 -0300

    feat: setup handlers on procedure endpoint

--- routes/procedure.js
+++ routes/procedure.js
@@ -2,20 +2,45 @@
 
 // read configured E-Com Plus app data
 const getConfig = require('./../lib/Api/AppConfig')
+// get all admin users from Store API
+const listAuthentications = require('./../lib/Api/ListAuthentications')
+// create new notification for each admin user
+const createNotification = require('./../lib/Api/CreateNotification')
 
 const POST = (id, meta, trigger, respond, storeId, appSdk) => {
   // get user notification options from E-Com Plus application data
+  let client
   getConfig({ appSdk, storeId })
 
     .then(({ authentications }) => {
-      if (authentications) {
-      }
+      // get authentication tokens
+      return appSdk.getAuth(storeId)
+    })
+
+    .then(auth => {
+      client = { appSdk, storeId, auth }
+      return listAuthentications(client)
+    })
+
+    .then(({ response }) => {
+      let { result } = response.data
+      let promises = []
+      result.forEach(authentication => {
+        let notification = {}
+        createNotification(client, authentication._id, notification)
+      })
+      return Promise.all(promises)
+    })
+
+    .then(() => {
+      // all done
+      respond('NOTIFY_SUCCESS')
     })
 
     .catch(err => {
       // cannot read app data
       // return error status code
-      respond({}, null, 500, 'APP_ERR', err.message)
+      respond({}, null, 500, 'STORE_API_ERR', err.message)
     })
 }
 
